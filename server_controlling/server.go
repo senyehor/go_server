29a6d4aa8d85a6ffdc4aa48278e632a2257bd096
Yehor Sencha <senyehor@gmail.com>
2023-03-24 11:59:49 +0200

    feat: made server able to run async

--- server_controlling/server.go
+++ server_controlling/server.go
@@ -4,37 +4,84 @@ import (
 	"github.com/maurice2k/tcpserver"
 	"github.com/senyehor/go_server/utils"
 	log "github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
 
-func CreateServer(f tcpserver.RequestHandlerFunc) *tcpserver.Server {
+const (
+	Running  = "running"
+	Inactive = "inactive"
+)
+
+type Server struct {
+	server  *tcpserver.Server
+	status  string
+	wg      sync.WaitGroup
+	handler tcpserver.RequestHandlerFunc
+}
+
+func (s *Server) Run() {
+	s.server = createServer(s.handler)
+	err := s.server.Listen()
+	if err != nil {
+		log.Error(err)
+		panic("Server failed to listen")
+	}
+	s.run()
+}
+
+func (s *Server) Stop() {
+	if s.isInactive() {
+		return
+	}
+	err := s.server.Shutdown(time.Second * 10)
+	s.wg.Done()
+	if err != nil {
+		log.Error(err)
+		panic("Server failed to shutdown")
+	}
+	s.setInactive()
+}
+
+func (s *Server) run() {
+	s.wg.Add(1)
+	s.setRunning()
+	go func(s *tcpserver.Server) {
+		err := s.Serve()
+		if err != nil {
+			log.Error(err)
+			panic("Server failed to start")
+		}
+	}(s.server)
+}
+
+func (s *Server) isRunning() bool {
+	return s.status == Running
+}
+func (s *Server) setRunning() {
+	s.status = Running
+}
+func (s *Server) setInactive() {
+	s.status = Inactive
+}
+func (s *Server) isInactive() bool {
+	return s.status == Inactive
+}
+
+func createServer(f tcpserver.RequestHandlerFunc) *tcpserver.Server {
 	server, err := tcpserver.NewServer(utils.AppConfig.ListenAddress())
 	if err != nil {
 		log.Error(err)
-		panic("Server failed to start")
+		panic("server creation failed")
 	}
 	server.SetRequestHandler(f)
 	return server
 }
 
-func RunServer(s *tcpserver.Server) {
-	err := s.Listen()
-	if err != nil {
-		log.Error(err)
-		panic("Server failed to start listening")
-	}
-	log.Info("Server successfully started")
-	err = s.Serve()
-	if err != nil {
-		log.Error(err)
-		panic("Server failed to serve")
-	}
-}
-
-func StopServer(s *tcpserver.Server) {
-	err := s.Shutdown(time.Second * 10)
-	if err != nil {
-		log.Error(err)
-		panic("Server failed to shutdown")
+func CreateServer(f tcpserver.RequestHandlerFunc) *Server {
+	return &Server{
+		server:  nil,
+		status:  Inactive,
+		handler: f,
 	}
 }
